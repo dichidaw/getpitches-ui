e12feed008c84214b0c31bb41e0f039a5054fe07
Michael Daw Balma <michael.daw@ui.ac.id>
2024-09-09 19:50:51 +0700

    feat(midi-connector): add midi list connector

---
+++ getpitches/src/Page/MidiConnector.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import WebMidiApi, {WebMidi, Input, Listener} from "webmidi";
+
+const MidiConnector: React.FC = () => {
+    const [devices, setDevices] = useState<string[]>([]);
+    const [note, setNote] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        let input: Input | undefined;
+
+        // Enable WebMIDI and list available devices
+        WebMidi.enable({ sysex: true })
+            .then(() => {
+                if (WebMidi.inputs.length < 1) {
+                    setDevices(["No device detected."]);
+                } else {
+                    setDevices(WebMidi.inputs.map((device) => device.name));
+
+                    // Attach event listener for the first input device
+                    input = WebMidi.getInputByName("LKMK3 MIDI");
+                    if (input) {
+                        input.addListener("midimessage",  (e: WebMidiApi.MessageEvent) => {
+                            if (e.data[0] !== 248) {
+                                console.log("MIDI Message Event:", e);
+                            }
+                        });
+                    }
+                }
+            })
+            .catch((err: Error) => {
+                setError(err.message);
+            });
+
+        // Cleanup function to disable WebMIDI and remove event listeners when the component unmounts
+        return () => {
+            if (input) {
+                input.removeListener("midimessage");
+            }
+            WebMidi.disable();
+        };
+    }, []);
+
+    return (
+        <div>
+            <h1>MIDI Devices</h1>
+            {error && <p>Error: {error}</p>}
+            {devices.length > 0 ? (
+                <ul>
+                    {devices.map((device, index) => (
+                        <li key={index}>{device}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No devices detected.</p>
+            )}
+            {note && <p>Last Note Played: {note}</p>}
+        </div>
+    );
+};
+
+export default MidiConnector;
