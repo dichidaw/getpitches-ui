7ada9bf9fdfef94a62683780117cfdfb2d5dec71
Michael Daw Balma <michael.daw@ui.ac.id>
2024-09-09 19:51:24 +0700

    feat(melody-tone): add midi playback

--- getpitches/src/Page/MelodyTone.tsx
+++ getpitches/src/Page/MelodyTone.tsx
@@ -1,57 +1,60 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import * as Tone from 'tone';
+import WebMidiApi, { Input, WebMidi } from 'webmidi';
+import { PitchDiff, ToneNote } from '../Types/ToneType';
+import { sampler } from '../Helper/ToneHelper';
 
 const MelodyTone: React.FC = () => {
-    const playSynthC = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("C4", "8n");
-    };
+    const [note, setNote] = useState<ToneNote | null>(null);
 
-    const playSynthD = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("D4", "8n");
-    };
+    const mod = (a: number, b: number): number => ((a % b) + b) % b;
 
-    const playSynthE = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("E4", "8n");
-    };
+    const handleMidiMessage = useCallback((e: WebMidiApi.MessageEvent) => {
+        if (e.data[0] === 156 && e.data[2] !== 0) {
+            setNote({
+                type: e.data[0],
+                pitch: e.data[1],
+                velocity: e.data[2],
+            });
+        }
+    }, []);
 
-    const playSynthF = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("F4", "8n");
-    };
+    useEffect(() => {
+        let input: Input | undefined;
 
-    const playSynthG = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("G4", "8n");
-    };
+        WebMidi.enable({ sysex: true })
+            .then(() => {
+                input = WebMidi.getInputByName('LKMK3 MIDI');
+                if (input) {
+                    input.addListener('midimessage', handleMidiMessage);
+                }
+            })
+            .catch((err: Error) => console.error(err.message));
 
-    const playSynthA = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("A4", "8n");
-    };
-    const playSynthB = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("B4", "8n");
-    };
-    const playSynthC5 = () => {
-        const synth = new Tone.Synth().toDestination();
-        synth.triggerAttackRelease("C5", "8n");
-        // synth.triggerAttackRelease("G4", "8n");
-        // synth.triggerAttackRelease("E5", "8n");
-    };
+        return () => {
+            if (input) input.removeListener('midimessage');
+            WebMidi.disable();
+        };
+    }, [handleMidiMessage]);
+
+    useEffect(() => {
+        if (note) {
+            const nt = mod(note.pitch - 60, 12);
+            const octave = Math.floor(note.pitch / 12) - 1;
+            const final = PitchDiff[nt] + octave;
+
+            Tone.loaded().then(() => {
+                sampler.triggerAttackRelease(final, 0.2, undefined, note.velocity / 127);
+            });
+            setNote(null);
+        }
+    }, [note, sampler]);
 
     return (
         <div>
-            <button onClick={playSynthC}>Play C4</button>
-            <button onClick={playSynthD}>Play D4</button>
-            <button onClick={playSynthE}>Play E4</button>
-            <button onClick={playSynthF}>Play F4</button>
-            <button onClick={playSynthG}>Play G4</button>
-            <button onClick={playSynthA}>Play A4</button>
-            <button onClick={playSynthB}>Play B4</button>
-            <button onClick={playSynthC5}>Play C5</button>
+            <button onClick={() => sampler.triggerAttackRelease('C4', 0.5)}>
+                Play C4
+            </button>
         </div>
     );
 };
